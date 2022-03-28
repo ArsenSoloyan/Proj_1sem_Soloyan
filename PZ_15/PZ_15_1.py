51ea5f240f01c4e170bef4107b46bce35c863c64
ArsenSoloyan <93132162+ArsenSoloyan@users.noreply.github.com>
2022-03-28 17:43:17 +0300

    Add files via upload

---
+++ PZ_15/PZ_15_1.py
@@ -0,0 +1,9 @@
+#В матрице найти минимальный элемент в предпоследней строке.
+
+from random import randint
+
+m, n, y, z = [int(input(i)) for i in ("Количество строк = ", "Количество столбцов = ", "От =", "До =")]
+matrix = [[randint(y, z) for _ in range(n)] for j in range(m)]
+for i in matrix:
+    print(*i)
+print('Минимальный элемент в предпоследней строке:', min(matrix[m-2]))
---
+++ PZ_15/PZ_15_2.py
@@ -0,0 +1,16 @@
+#В квадратной матрице элементы на главной диагонали увеличить в 2 раза.
+
+from random import randint
+import numpy as np
+
+m, n, y, z = [int(input(i)) for i in ("Количество строк = ", "Количество столбцов = ", "От =", "До =")]
+matrix = [[randint(y, z) for _ in range(n)] for j in range(m)]
+print('Исходная матрица:')
+for i in matrix:
+    print(*i)
+h = np.diagonal(matrix)
+for x in range(0, len(matrix)):
+    matrix[x][x] = h[x] * 2
+print('Полученная матрица:')
+for i in matrix:
+    print(*i)
